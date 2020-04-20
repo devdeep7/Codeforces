a650b16c67a65c0361be3c3dcbba247eed1cc27d
Devdeep Brahmbhatt <52355787+devdeep7@users.noreply.github.com>
2020-04-20 00:52:53 -0400

    Update Gennady and a card game.cpp

--- Gennady and a card game.cpp
+++ Gennady and a card game.cpp
@@ -1,29 +1,45 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-
-
-
-
-
+ 
+ 
+ 
+int playorNot(string *OnDeck, string *InHandCards, int l1, int l2){
+    int i;
+    int j;
+    for (i=0; i<l1; i++){
+        for (j=0; j<l2; j++){
+            if ((*OnDeck)[i]==(*InHandCards)[j]){
+                return 1;
+                
+            }
+        }
+    }
+        
+    return 0;
+    
+}
+ 
 int main(){
     
     int l1;
     int l2;
     int y;
-    int i;
-    int num;
-    string number;
+    string OnDeck;
+    string InHandCards;
     
+    getline(cin, OnDeck);
+    getline(cin, InHandCards);
     
-    cin>>num;
-    
-    for(i=0;i<num;i++){
-    cin>>number.append()
-    }
+    l1 = OnDeck.size();
+    l2 = InHandCards.size();
     
     y = playorNot(&OnDeck, &InHandCards, l1, l2);
-
-   
+ 
+    if(y==1){
+        cout<<"YES"<<endl;
+    }
+    else
+        cout<<"NO"<<endl;
     
 }
